49ddf4a75c929bd875631c95b3e78ee02c018348
gfranco42 <franco.guillaume83@gmail.com>
2019-10-27 14:53:10 +0100

    add UVs on boxes

--- src/shapes/box.c
+++ src/shapes/box.c
@@ -33,6 +33,40 @@ static t_vec3	box_normal(t_vec3 p, t_vec3 a, t_vec3 b)
 
  #include "stdio.h"
 
+static void		box_uvs(double *u, double *v, t_vec3 normal, t_vec3 dir)
+{
+	if (normal.x > 0)
+	{
+		*u = 0.5 - (dir.z / (2 * dir.x));
+		*v = 0.5 - (dir.y / (2 * dir.x));
+	}
+	else if (normal.x < 0)
+	{
+		*u = 0.5 - (dir.z / (2 * dir.x));
+		*v = 0.5 + (dir.y / (2 * dir.x));
+	}
+	else if (normal.y > 0)
+	{
+		*u = 0.5 + (dir.x / (2 * dir.y));
+		*v = 0.5 + (dir.z / (2 * dir.y));
+	}
+	else if (normal.y < 0)
+	{
+		*u = 0.5 - (dir.x / (2 * dir.y));
+		*v = 0.5 + (dir.z / (2 * dir.y));
+	}
+	else if (normal.z > 0)
+	{
+		*u = 0.5 + (dir.x / (2 * dir.z));
+		*v = 0.5 - (dir.y / (2 * dir.z));
+	}
+	else if (normal.z < 0)
+	{
+		*u = 0.5 + (dir.x / (2 * dir.z));
+		*v = 0.5 + (dir.y / (2 * dir.z));
+	}
+}
+
 struct s_hit	hit_box(struct s_ray ray, struct s_box *box, struct s_intersection_tab *intersections)
 {
 	t_vec3	invdir;
@@ -40,6 +74,10 @@ struct s_hit	hit_box(struct s_ray ray, struct s_box *box, struct s_intersection_
 	double	max;
 	t_vec3	cmin;
 	t_vec3	cmax;
+	t_vec3	normal;
+	double	u;
+	double	v;
+	t_vec3	p;
 
 	invdir = vec3_invert(ray.direction);
 	cmin = vec3(
@@ -81,9 +119,14 @@ struct s_hit	hit_box(struct s_ray ray, struct s_box *box, struct s_intersection_
 			};
 
 		}
+	p = ray_point_at(&ray, min);
+	normal = box_normal(p, box->bounds[0], box->bounds[1]);
+	box_uvs(&u, &v, normal, vec3_unit(p));
 	return ((struct s_hit) {
 		.t = min,
-		.normal = box_normal(ray_point_at(&ray, min), box->bounds[0], box->bounds[1])
+		.normal = normal,
+		.u = u,
+		.v = v
 	});
 }
 
