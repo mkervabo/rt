5c3a5a15464bb0c7df331076993cad7d421dce9d
Maiwenn KERVABON <mkervabo@student.42.fr>
2019-10-29 10:21:09 +0100

    add read color in read light super

--- src/lights/utils.c
+++ src/lights/utils.c
@@ -1,6 +1,32 @@
 #include "utils.h"
 #include "config_utils.h"
 
+
+static bool	read_color(t_toml_table *toml, t_color *color)
+{
+	t_toml	*value;
+
+	if (!(value = table_get(toml, "r")))
+		color->r = 0;
+	else if (value->type != TOML_Integer)
+		return ((bool)rt_error(NULL, "Is not a interger in red color"));
+	else
+		color->r = value->value.integer_v;
+	if (!(value = table_get(toml, "g")))
+		color->g = 0;
+	else if (value->type != TOML_Integer)
+		return ((bool)rt_error(NULL, "Is not a interger in green color"));
+	else
+		color->g = value->value.integer_v;
+	if (!(value = table_get(toml, "b")))
+		color->b = 0;
+	else if (value->type != TOML_Integer)
+		return ((bool)rt_error(NULL, "Is not a interger in blue color"));
+	else
+		color->b = value->value.integer_v;
+	return (true);
+}
+
 bool		read_light_super(t_toml_table *toml, t_light *light)
 {
 	t_toml		*value;
@@ -9,6 +35,9 @@ bool		read_light_super(t_toml_table *toml, t_light *light)
 		light->intensity = 1;
 	else if (!read_digit(value, &light->intensity))
 		return ((bool)rt_error(NULL, "Is not a number"));
-	light->color = (t_color) { 255, 255, 255 };
-	return true;
+	if (!(value = table_get(toml, "color")))
+		light->color = (t_color) { 255, 255, 255 };
+	else if (!read_color(value->value.table_v, &light->color))
+		return ((bool)rt_error(NULL, "Invalid color in read light super"));
+	return (true);
 }
