8bbf1880c1b775e820a361024ba639ad07df013a
David DE JESUS DUARTE <dde-jesu@student.42.fr>
2019-11-13 20:42:35 +0100

    Update torus script

--- scripts/torus.js
+++ scripts/torus.js
@@ -4,7 +4,20 @@ const MAJOR = 0.5;
 const MINOR = 0.2;
 const Z = -3;
 
-const MAX = 100;
+const MAX = 250;
+
+console.log(`
+name = "Torus ${MAX}"
+width = 1280
+height = 800
+
+[camera]
+	type = "PERSPECTIVE"
+	position.x = 1.25
+	position.y = -1.25
+	rotation.y = 20
+	rotation.x = 20
+`)
 
 for (let i = 0; i < MAX; i++) {
 	const x = MAJOR * Math.cos(i / MAX * Math.PI * 2);
@@ -13,7 +26,7 @@ for (let i = 0; i < MAX; i++) {
 	console.log(`
 		[[objects]]
 			type = "SPHERE"
-			material = "UV"
+			material.type = "NORMAL"
 			position = { x = ${x.toFixed(2)}, y = ${y.toFixed(2)}, z = ${Z} }
 			radius = ${MINOR}
 	`);
